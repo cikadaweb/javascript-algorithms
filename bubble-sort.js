c4ebfb3936730bc71164d1c2b90c4b81bce436f0
cikada <adel.garipovazn@mail.ru>
2023-04-18 19:11:21 +0300

    Add bubble-sort

---
+++ bubble-sort.js
@@ -0,0 +1,16 @@
+// BubbleSort
+// mas = [1, -2, 0, -3];
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < mas.length - 1 - i; j++) {  // -i чтобы не пробегаться по уже отсортированным элементам
+//     console.log("Compare it mas[j]: ", mas[j]);
+//     console.log("Compare with mas[j + 1]: ", mas[j + 1]);
+//     console.log("Original mas: ", mas);
+//     if (mas[j] > mas[j + 1]) {
+//       temp = mas[j];
+//       mas[j] = mas[j + 1];
+//       mas[j + 1] = temp;
+//     }
+//     console.log("Sorted array: ", mas, "\n");
+//   }
+// }
+// console.log(mas);
