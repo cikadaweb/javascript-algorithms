bf5ee14e6cddee57409a843a0d05758db4145fc0
cikada <adel.garipovazn@mail.ru>
2023-04-22 13:17:37 +0300

    Update bubble-sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,16 +1,47 @@
 // BubbleSort
+mas = [1, -2, 0, -3, -5, -6, 100, 4, 2];
+
+bubbleSort(mas);
+
+function bubbleSort(mas) {
+  for (let i = 0; i < mas.length - 1; i++) {
+    for (let j = 0; j < mas.length - 1 - i; j++) {
+      // -i чтобы не пробегаться по уже отсортированным элементам
+      if (mas[j] > mas[j + 1]) {
+        temp = mas[j];
+        mas[j] = mas[j + 1];
+        mas[j + 1] = temp;
+      }
+    }
+  }
+}
+
+console.log(mas);
+
+// На каждой итерации будет сортироваться 1 элемент
 // mas = [1, -2, 0, -3];
-// for (let i = 0; i < 3; i++) {
-//   for (let j = 0; j < mas.length - 1 - i; j++) {  // -i чтобы не пробегаться по уже отсортированным элементам
-//     console.log("Compare it mas[j]: ", mas[j]);
-//     console.log("Compare with mas[j + 1]: ", mas[j + 1]);
-//     console.log("Original mas: ", mas);
-//     if (mas[j] > mas[j + 1]) {
-//       temp = mas[j];
-//       mas[j] = mas[j + 1];
-//       mas[j + 1] = temp;
-//     }
-//     console.log("Sorted array: ", mas, "\n");
+// console.log(mas);
+// for (let i = 0; i < mas.length - 1; i++) {
+//   if (mas[i] > mas[i + 1]) {
+//     temp = mas[i + 1];
+//     mas[i + 1] = mas[i];
+//     mas[i] = temp;
+//   }
+// }
+// console.log(mas);
+// for (let i = 0; i < mas.length - 1; i++) {
+//   if (mas[i] > mas[i + 1]) {
+//     temp = mas[i + 1];
+//     mas[i + 1] = mas[i];
+//     mas[i] = temp;
+//   }
+// }
+// console.log(mas);
+// for (let i = 0; i < mas.length - 1; i++) {
+//   if (mas[i] > mas[i + 1]) {
+//     temp = mas[i + 1];
+//     mas[i + 1] = mas[i];
+//     mas[i] = temp;
 //   }
 // }
 // console.log(mas);
